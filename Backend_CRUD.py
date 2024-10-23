e672bb322918912bf057c3d19893b53e32758532
Guilherme <guilherme.santos300106@gmail.com>
2024-10-23 20:19:58 -0300

    Save

--- Backend_CRUD.py
+++ Backend_CRUD.py
@@ -3,11 +3,20 @@ import openpyxl
 import sqlite3
 
 
-tabela = pd.read_excel('C:/Users/Guilherme/Desktop/Projetos/Trabalho-Engenharia-de-sotfware/base_dados/Base Vendas - 2022.xlsx')
+tabela_df = pd.read_excel('C:/Users/Guilherme/Desktop/Projetos/Trabalho-Engenharia-de-sotfware/base_dados/Base Vendas - 2022.xlsx')
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 1500)
 
 
 
-print(tabela)
+
+
+
+
+
+
+
+print(tabela_df)
 
 
 
